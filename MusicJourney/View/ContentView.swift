fec9b41ad8ecdf0be73ccaad78270222a9a13dd9
Lucas Rafael Dias <lucas.rafael8445@gmail.com>
2026-06-05 16:09:18 -0400

    Tela de formulario

--- MusicJourney/View/ContentView.swift
+++ MusicJourney/View/ContentView.swift
@@ -6,15 +6,35 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [],
+        animation: .default)
+    private var users: FetchedResults<User>
+
+    var body: some View {
+        Group {
+            if users.isEmpty {
+                OnboardingFormView()
+            } else {
+                MainTabView()
+            }
+        }
+    }
+}
+
+struct MainTabView: View {
     var body: some View {
         TabView {
             JourneyView()
                 .tabItem {
                     Label("Jornada", systemImage: "map")
                 }
-            
+
             HistoryView()
                 .tabItem {
                     Label("Histórico", systemImage: "clock.fill")
