eb81c7f9c84e320e8a781a6123f6e53a12891ab9
DaviBrasil <davi.brasil2005@gmail.com>
2026-06-05 13:44:21 -0400

    first commit

---
+++ MusicJourney/View/HistoryView.swift
@@ -0,0 +1,16 @@
+//
+//  HistoryView.swift
+//  MusicJourney
+//
+//  Created by Academy on 05/06/26.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text("Histórico")
+    }
+}
+
+
---
+++ MusicJourney/View/JourneyView.swift
@@ -0,0 +1,15 @@
+//
+//  JourneyView.swift
+//  MusicJourney
+//
+//  Created by Academy on 05/06/26.
+//
+
+import SwiftUI
+
+struct JourneyView: View {
+    var body: some View {
+        Text("Jornada")
+    }
+}
+
