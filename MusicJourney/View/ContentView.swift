2eb52a43bbd80f8c914ee79e1cc9a5960e0655eb
Lucas Rafael Dias <lucas.rafael8445@gmail.com>
2026-06-05 16:26:52 -0400

    Revert "Merge pull request #3 from Davibrasil05/lucas-initialviews"
    This reverts commit 5cf9e8c6251a7840e8b80a6c5167736898c7b8c6, reversing
    changes made to 07595d2a42e6d821e9274e6e1462c54a9af442f2.

--- MusicJourney/View/ContentView.swift
+++ MusicJourney/View/ContentView.swift
@@ -5,36 +5,16 @@
 //  Created by Academy on 05/06/26.
 //
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [],
-        animation: .default)
-    private var users: FetchedResults<User>
-
-    var body: some View {
-        Group {
-            if users.isEmpty {
-                OnboardingFormView()
-            } else {
-                MainTabView()
-            }
-        }
-    }
-}
-
-struct MainTabView: View {
     var body: some View {
         TabView {
             ObjectiveListView()
                 .tabItem {
                     Label("Jornada", systemImage: "map.fill")
                 }
-
-            HistoryView()
+            
+            UserProfileView()
                 .tabItem {
                     Label("Perfil", systemImage: "person.crop.circle.fill")
                 }
