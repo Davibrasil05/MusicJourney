4007c323309f68fc5e8c3d24cb2bcc5398b846cd
Davi Brasi <davi.brasil2005@gmail.com>
2026-07-16 17:31:31 -0400

    refactor: secrets swift

---
+++ MusicJourney/Secrets.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/// Armazena as chaves de API com segurança local.
+/// ESTE ARQUIVO DEVE ESTAR NO .gitignore!
+enum Secrets {
+    // Chave da OpenAI para geração de metas com IA
+    static let openAIKey = ""
+
+}
